960c7994f650f9b83767a50586b6e728c915c1fb
Zou <rickyzou2@gmail.com>
2022-09-24 08:46:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'Zou',
   email: 'RickyZou2@gmail.com',
   link: 'https://zyq.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '我的碎碎念.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
@@ -17,9 +17,9 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/realrealzou',
+  socialLink: 'https://linktr.ee/rzou',
   seo: {
-    keywords: ['Blog', 'Zou', 'Notion'],
+    keywords: ['Blog', 'Zou', 'RealZou'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
